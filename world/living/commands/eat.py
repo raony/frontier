c507eb815c5a1ed7289f0aadacad4d299827347b
Raony Araujo <delvar@gmail.com>
2025-08-25 11:24:49 -0300

    tyding up eat cmd

--- world/living/commands/eat.py
+++ world/living/commands/eat.py
@@ -1,10 +1,23 @@
 """Eat command for consuming Food objects."""
 
 from commands.command import Command
+from world.utils import DisplayNameWrapper
 
 
 class CmdEat(Command):
-    """Eat an edible item to reduce hunger."""
+    """Eat an edible item to reduce hunger.
+
+    Usage:
+      eat <item>
+
+    Examples:
+      eat apple
+      eat bread
+      eat roasted chicken
+
+    Consumes a portion of food from your inventory to reduce hunger.
+    Most foods can be eaten in multiple portions.
+    """
 
     key = "eat"
     locks = "cmd:all()"
@@ -14,26 +27,21 @@ class CmdEat(Command):
         target = self.args.strip()
 
         if not target:
-            caller.msg("What do you want to eat?")
-            return
+            return caller.msg("What do you want to eat?")
 
         obj = caller.search_item(target)
         if not obj:
-            caller.msg(f"You don't have {target}.")
-            return
+            return caller.msg(f"You don't have {target}.")
         if not hasattr(obj, "food"):
-            caller.msg("You can't eat that.")
-            return
+            return caller.msg("You can't eat that.")
         if not obj.food.can_eat(caller):
-            caller.msg("It doesn't look edible.")
-            return
+            return caller.msg("It doesn't look edible.")
         if obj.food.eaten_percentage == 1:
-            caller.msg(f"There's nothing left of {obj.get_display_name(caller)} to eat.")
-            return
+            return caller.msg(f"There's nothing left of {self.get_display_name(obj)} to eat.")
 
         obj.food.eat(caller)
         caller.location.msg_contents(
-            f"$You() $conj(eat) a portion of $obj(obj).",
-            mapping={"obj": obj},
-            from_obj=caller
+            "$You() $conj(eat) a portion of $obj(food).",
+            from_obj=caller,
+            mapping={"food": DisplayNameWrapper(obj, command_narration=True)},
         )
