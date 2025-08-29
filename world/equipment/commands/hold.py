60ed8d3ef79ee9f66aae2de731790dfe9bf0d151
Raony Araujo <delvar@gmail.com>
2025-08-28 22:21:52 -0300

    apply standard to hold

--- world/equipment/commands/hold.py
+++ world/equipment/commands/hold.py
@@ -30,7 +30,7 @@ class CmdHold(Command):
             return caller.msg(f"You can't find {item_key}.")
 
         if not caller.held_items.is_holdable(obj):
-            return caller.msg(f"You can't hold {obj.get_display_name(caller)}.")
+            return caller.msg(f"You can't hold {self.get_display_name(obj)}.")
 
         if not slot_keys:
             current_slots = caller.held_items.get_slots_for(obj)
@@ -44,10 +44,10 @@ class CmdHold(Command):
                 slot_keys = [current_slots[0]]
 
         if caller.held_items.is_too_heavy(obj, slot_keys):
-            return caller.msg(f"{obj.get_display_name(caller)} is too heavy for you to hold.")
+            return caller.msg(f"{self.get_display_name(obj)} is too heavy for you to hold.")
 
         if caller.held_items.is_already_holding(obj, slot_keys):
-            return caller.msg(f"You are already holding {obj.get_display_name(caller)}.")
+            return caller.msg(f"You are already holding {self.get_display_name(obj)}.")
 
         if not caller.held_items.is_slots_available(obj, slot_keys):
             return caller.msg(f"You don't have enough free hands.")
@@ -60,11 +60,11 @@ class CmdHold(Command):
                 slot_display = slot_keys[0]
             else:
                 slot_display = "both hands"
-            caller.location.msg_contents(
-                f"$You() $conj(hold) $you(obj) with {slot_display}",
-                from_obj=caller,
-                mapping={"obj": obj}
+
+            self.send_room_message(
+                f"$You() $conj(hold) $obj(item) with {slot_display}",
+                mapping={"item": obj}
             )
             caller.held_items.add(obj, slots=slot_keys)
         else:
-            caller.msg(f"You can't hold {obj.get_display_name(caller)}.")
+            caller.msg(f"You can't hold {self.get_display_name(obj)}.")
